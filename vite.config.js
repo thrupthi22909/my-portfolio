1922fc00a9fc3c36b4ff0951ec9aee066b718107
Thrupthi H R <thrupthi.hr22@gmail.com>
2025-06-10 12:31:24 +0530

    Fix: Removed lightningcss and aligned Tailwind/PostCSS setup for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
+// replace 'your-username' and 'repo-name' accordingly
 export default defineConfig({
+  base: '/my-portfolio/', // this is your repo name
   plugins: [react()],
-})
+});
