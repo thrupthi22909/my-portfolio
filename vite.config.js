ac9ef7c684b0b2ae719b49fea9d0f7074b0068f7
Thrupthi H R <thrupthi.hr22@gmail.com>
2025-06-10 21:23:03 +0530

    Fix: add @vitejs/plugin-react for Vercel build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,6 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// replace 'your-username' and 'repo-name' accordingly
 export default defineConfig({
-  base: '/my-portfolio/', // this is your repo name
   plugins: [react()],
-});
+})
